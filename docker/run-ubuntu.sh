e92b560c1053c02a8d25d29827eb0e36b130e0d9
hud <29axe@users.noreply.github.com>
2017-04-20 18:55:25 +0200

    Update docker documentation for ubuntu and clean repository

--- docker/run-ubuntu.sh
+++ docker/run-ubuntu.sh
@@ -2,26 +2,21 @@
 BASEDIR=$(dirname $PWD)
 IMAGE_NAME='hotmaps/building_h-c_default_eu28'
 
-# check dependencies
-if ! hash "docker-machine" 2>/dev/null;
-then
-    echo "docker-machine can't be found"
-    exit 1
-fi
-# get first machine name
-#DOCKER_MACHINE_NAME=$(docker-machine ls --format "{{.Name}}" | head -1)
-#echo $machine_name
-
-if [[ -z "$DOCKER_MACHINE_NAME" ]]; then
-    echo "DOCKER_MACHINE_NAME environment variable isn't set. Have you run docker-machine env?"
-    exit 1
-fi
-
 # update the compose files
-ip=$(docker-machine ip $DOCKER_MACHINE_NAME)
+ip='localhost'
 
-echo ip for $DOCKER_MACHINE_NAME is $ip
+echo ip for docker is $ip
 echo Creating container...
 
-docker run -d -v "${BASEDIR}/code:/data" -p 8181:80 -it ${IMAGE_NAME}
-echo "Container creation script executed correctly (make sur the new container was successfuly added)"
+id=$(docker run -d -v "${BASEDIR}/code:/data" -p 8181:80 -it ${IMAGE_NAME})
+exit_cod=$?
+
+if [ ${exit_code} = 0 ]
+    then
+	    echo "Container creation script executed correctly (make sur the new container was successfuly added)"
+		echo "Container's id: ${id}"
+	else
+	    echo "An error occurred! (exit code: ${exit_code})"
+		docker stop ${id} >/dev/null
+		docker rm ${id} >/dev/null
+fi
